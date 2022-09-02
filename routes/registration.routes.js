f9baa4551b38760dad9869879ebbb08874f81522
Jared Mejia <jaredmejia4440@gmail.com>
2022-09-01 20:49:10 -0600

    verify registration now is a middleware

--- routes/registration.routes.js
+++ routes/registration.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 
+//import controllers
 const {
   getAllRegistrations,
   getOneRegistration,
@@ -8,16 +9,19 @@ const {
   cancelRegistration,
 } = require("../controllers/registration.controller");
 
+//import middlewares
+const { userExist } = require("../middleware/registration.middleware");
+
 const registrationRouter = express.Router();
 
 registrationRouter.get("/", getAllRegistrations);
 
-registrationRouter.get("/:id", getOneRegistration);
+registrationRouter.get("/:id", userExist, getOneRegistration);
 
 registrationRouter.post("/", markEntryTime);
 
-registrationRouter.patch("/:id", markExitTime);
+registrationRouter.patch("/:id", userExist, markExitTime);
 
-registrationRouter.delete("/:id", cancelRegistration);
+registrationRouter.delete("/:id", userExist, cancelRegistration);
 
 module.exports = { registrationRouter };
